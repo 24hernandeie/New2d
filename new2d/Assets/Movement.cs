82862c81217bfdbb70b9b491a668869f754d6ff0
24hernandeie <99415808+24hernandeie@users.noreply.github.com>
2023-11-30 08:48:16 -0600

    g

--- new2d/Assets/Movement.cs
+++ new2d/Assets/Movement.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public playerScript controller;
+    public Animator animator;
+    
+    public float runSpeed = 40f;
+    
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,26 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("jump"))
+        {
+            jump = true;
+        }   
+        if (Input.GetButtonDown("Crouch"))
+        {
+            Crouch = true;
+        }
+        else if (Input.GetButtonUp("crouch"))
+        {
+            crouch = flse;
+        }
+    }
+
+    void FixedUpdate ()
+    {
+
     }
 }
